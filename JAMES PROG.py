2d6a1450333aeffd6de2f6fc3e21033eb84be2e4
James Tagalog <136965759+JamesTagalog@users.noreply.github.com>
2023-09-20 05:31:15 -0700

    Add files via upload

---
+++ JAMES PROG.py
@@ -0,0 +1,9 @@
+input("Fullname:")
+input("ID#:")
+input("Course and Section:")
+Q1=eval(input("Q1:"))
+Q2=eval(input("Q2:"))
+Q3=eval(input("Q3:"))
+Q4=eval(input("Q4:"))
+sum= (Q1 + Q2 + Q3 + Q4)/ 4
+print("the sum of", Q1, "+", Q2, "+", Q3, "+", Q4, "is", sum)
